db7fe3ada244fbc247324c6e72fb275dadc5f90b
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-11-23 00:17:25 -0600

    02-modulos

---
+++ 02-Modulos/CommonJS/app.js
@@ -0,0 +1,8 @@
+const calculadora = require("./calculadora.js"),
+c = console.log
+
+c(calculadora.sumar(8,5))
+c(calculadora.restar(10,8))
+c(calculadora.multiplicar(4,5)),
+c(calculadora.dividir(8,6))
+c(calculadora.modulo(8,4))
---
+++ 02-Modulos/ESModules/app.mjs
@@ -0,0 +1,8 @@
+import { calculadora } from "./calculadora.mjs"
+
+const c = console.log
+c(calculadora.sumar(8,5))
+c(calculadora.restar(10,8))
+c(calculadora.multiplicar(4,5)),
+c(calculadora.dividir(8,6))
+c(calculadora.modulo(8,4))
