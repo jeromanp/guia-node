66f3e75b4bbe33d4f2b90e4e6edfaa2f45ec5908
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-11-29 12:11:38 -0600

    add view pug files

--- 09-ExpressMVC/app.js
+++ 09-ExpressMVC/app.js
@@ -14,6 +14,9 @@ app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
