d42cea3cbdd254d8e8273c54df351fa0caae462d
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-11-30 00:22:57 -0600

    error controller

--- 09-ExpressMVC/controllers/errorController.js
+++ 09-ExpressMVC/controllers/errorController.js
@@ -1,10 +1,10 @@
 const error404 = (req, res) => {
-    res.status(404).render("error", {
-      title: "Error 404 Not Found",
-      message: "El recurso que estás buscando no existe.",
-    });
-  };
-  
-  export default {
-    error404,
-  };
+  res.status(404).render("error", {
+    title: "Error 404 Not Found",
+    message: "El recurso que estás buscando no existe.",
+  });
+};
+
+export default {
+  error404,
+};
