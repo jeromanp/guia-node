a5cce7d2e5d47c5a178b80656c4bdc45067ee210
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-11-30 00:56:29 -0600

    fix view engine

--- 09-ExpressMVC/app.js
+++ 09-ExpressMVC/app.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.set("views", path.join(__dirname, "views"));
+app.set("/views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 app.use(express.static(path.join(__dirname, "public")));
